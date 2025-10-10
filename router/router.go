79f9dc204c8b3e5e7dbda6f6145c187bc50f9bb9
DwiFirdaus08 <kirey.zaindeltaperguson@gmail.com>
2025-10-10 14:11:17 +0700

    route

--- router/router.go
+++ router/router.go
@@ -9,12 +9,17 @@ import (
 func SetupRouter() *mux.Router {
 	router := mux.NewRouter()
 
-	apiRouter := router.PathPrefix("/api").Subrouter()
+	// apiRouter := router.PathPrefix("/api").Subrouter()
 
-	apiRouter.HandleFunc("/jalans", handler.GetAllJalanHandler).Methods("GET")
-	apiRouter.HandleFunc("/jalans", handler.CreateJalanHandler).Methods("POST")
-	apiRouter.HandleFunc("/jalans/{id}", handler.UpdateJalanHandler).Methods("PUT")
-	apiRouter.HandleFunc("/jalans/{id}", handler.DeleteJalanHandler).Methods("DELETE")
+	// apiRouter.HandleFunc("/jalans", handler.GetAllJalanHandler).Methods("GET")
+	// apiRouter.HandleFunc("/jalans", handler.CreateJalanHandler).Methods("POST")
+	// apiRouter.HandleFunc("/jalans/{id}", handler.UpdateJalanHandler).Methods("PUT")
+	// apiRouter.HandleFunc("/jalans/{id}", handler.DeleteJalanHandler).Methods("DELETE")
+
+	router.HandleFunc("/jalans", handler.GetAllJalanHandler).Methods("GET")
+	router.HandleFunc("/jalans", handler.CreateJalanHandler).Methods("POST")
+	router.HandleFunc("/jalans/{id}", handler.UpdateJalanHandler).Methods("PUT")
+	router.HandleFunc("/jalans/{id}", handler.DeleteJalanHandler).Methods("DELETE")
 
 	return router
 }
